6d5ccfd1ecb997b5d63840d6c742abb26f4765be
tangyinze <706945128@qq.com>
2025-03-12 00:13:36 +0800

    spring cloud demo test

---
+++ services/service-order/src/main/java/com/tyz/order/OrderMainApplication.java
@@ -0,0 +1,17 @@
+package com.tyz.order;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @program: cloud-demo
+ * @description: spring cloud order test
+ * @author: tyz
+ * @create: 2025-03-07
+ */
+@SpringBootApplication
+public class OrderMainApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(OrderMainApplication.class, args);
+    }
+}
---
+++ services/service-product/src/main/java/com/tyz/product/ProductApplication.java
@@ -0,0 +1,17 @@
+package com.tyz.product;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @program: cloud-demo
+ * @description: product service test
+ * @author: tyz
+ * @create: 2025-03-07
+ */
+@SpringBootApplication
+public class ProductApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(ProductApplication.class, args);
+    }
+}
